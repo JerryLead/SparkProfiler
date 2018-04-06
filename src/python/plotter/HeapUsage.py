c98bedf2d8610b1761661fcbe7b5b9ffeb7000ce
JerryLead <csxulijie@gmail.com>
2018-04-06 21:52:47 +0800

    Can plot the heap usage and gc pause of the three collectors

--- src/python/plotter/HeapUsage.py
+++ src/python/plotter/HeapUsage.py
@@ -58,7 +58,7 @@ class HeapUsage:
         commaIndex = line.find(',', commaIndex + 1)
         allocated = float(line[line.find('allocated') + 12: commaIndex])
         commaIndex = line.find(',', commaIndex + 1)
-        gcPause = float(line[line.find('gcPause') + 12: commaIndex - 1])
+        gcPause = float(line[line.find('gcPause') + 10: commaIndex - 1])
         gcCause = line[line.find('gcCause') + 10: ]
 
         return Usage(gcType, time, beforeGC, afterGC, allocated, gcPause, gcCause)
@@ -138,6 +138,34 @@ class HeapUsage:
                 genUsage.append(usage.getUsage())
         return (genTime, genUsage)
 
+    def getGCPauseAndTime(self):
+        ygcTime = []
+        ygcPause = []
+        fgcTime = []
+        fgcPause = []
+        gcTimeSet = set()
+
+        for usage in self.youngGen:
+            if (usage.getGCType() == "FGC"):
+                fgcTime.append(usage.getTime())
+                fgcPause.append(usage.getGCPause())
+                gcTimeSet.add(usage.getTime())
+            elif (usage.getGCType() == "YGC"):
+                ygcTime.append(usage.getTime())
+                ygcPause.append(usage.getGCPause())
+                gcTimeSet.add(usage.getTime())
+
+        for usage in self.oldGen:
+            time = usage.getTime()
+            if (time not in gcTimeSet):
+                if (usage.getGCType() == "FGC"):
+                    fgcTime.append(usage.getTime())
+                    fgcPause.append(usage.getGCPause())
+                elif (usage.getGCType() == "YGC"):
+                    ygcTime.append(usage.getTime())
+                    ygcPause.append(usage.getGCPause())
+        return (ygcTime, ygcPause, fgcTime, fgcPause)
+
 
 def plotHeapUsage(appName, gclogFile, outputFile):
 
@@ -154,22 +182,23 @@ def plotHeapUsage(appName, gclogFile, outputFile):
     axes[0].set_ylabel("Young Gen (MB)")
     axes[1].set_ylabel("Old Gen (MB)")
 
-    # axes[0].set_ylim(0, 1500)  # The ceil
-    # axes[1].set_ylim(0, 5000)  # The ceil
+    axes[0].set_ylim(0, 3000)  # The ceil
+    axes[1].set_ylim(0, 7000)  # The ceil
     # axes[0].set_xlim(0, 5000)
 
-    colors = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
+    #colors = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
 
-    axes[0].plot(heapUsage.getGenTime("Young"), heapUsage.getGenBeforeGC("Young"), '-o', label='BeforeGC', markersize=0.8, color=colors[0])
-    axes[0].plot(heapUsage.getGenTime("Young"), heapUsage.getGenAfterGC("Young"), '-*', label='AfterGC', color=colors[1])
+    #colors = [u'#009999', u'#cc0033', u'#663366']
+
+    axes[0].plot(heapUsage.getGenTime("Young"), heapUsage.getGenBeforeGC("Young"), '--o', linewidth=0.9, label='BeforeGC', markersize=0.9)
+    axes[0].plot(heapUsage.getGenTime("Young"), heapUsage.getGenAfterGC("Young"), '-*', linewidth=0.9, label='AfterGC', markersize=0.9)
     allocated = heapUsage.getGenAllocated("Young")
-    axes[0].plot(allocated[0], allocated[1], '-', label='Allocated', color=colors[2])
+    axes[0].plot(allocated[0], allocated[1], '-', label='Allocated')
 
-
-    axes[1].plot(heapUsage.getGenTime("Old"), heapUsage.getGenBeforeGC("Old"), '-o', label='BeforeGC', markersize=1, color=colors[0])
-    axes[1].plot(heapUsage.getGenTime("Old"), heapUsage.getGenAfterGC("Old"), '-*', label='AfterGC', color=colors[1])
+    axes[1].plot(heapUsage.getGenTime("Old"), heapUsage.getGenBeforeGC("Old"), '--o', linewidth=0.9, label='BeforeGC', markersize=0.9)
+    axes[1].plot(heapUsage.getGenTime("Old"), heapUsage.getGenAfterGC("Old"), '-*', linewidth=0.9, label='AfterGC', markersize=0.9)
     allocated = heapUsage.getGenAllocated("Old")
-    axes[1].plot(allocated[0], allocated[1], '-', label='Allocated', color=colors[2])
+    axes[1].plot(allocated[0], allocated[1], '-', label='Allocated')
 
 
     axes[0].grid(False)
@@ -179,12 +208,31 @@ def plotHeapUsage(appName, gclogFile, outputFile):
     axes[0].set_xlim(xmin=0)
     axes[1].set_xlim(xmin=0)
     axes[0].get_xaxis().set_visible(False)
+
+    axes[0].legend(loc='upper left')
+
+    # draw GCPause time bar plot
+
+    (ygcTime, ygcPause, fgcTime, fgcPause) = heapUsage.getGCPauseAndTime()
+
+    colors2 = [u'#DDA0DD', u'#6A5ACD', u'#A9A9A9', u'#ADD8E6', u"#cc3333"]
+    axes3 = axes[1].twinx()
+
+    bar1 = axes3.bar(ygcTime, ygcPause, 0.01, color=colors2[2], label="YGC Pause", edgecolor=colors2[2])
+    bar2 = axes3.bar(fgcTime, fgcPause, 0.005, color=colors2[4], label="FGC Pause", edgecolor=colors2[4])
+
+    axes3.set_ylabel(r"GC pause time (sec)")
+    axes[1].set_xlabel("Time (sec)")
+    ymin, ymax = axes3.get_ylim()
+    axes3.set_ylim(ymin, ymax * 2)
+
     plt.suptitle(appName)
-    plt.legend(loc='lower right')
+    #axes[1].legend(loc='upper left')
+    axes3.legend(loc='upper left')
 
     fig = plt.gcf()
-    plt.show()
-    # fig.savefig(outputFile, dpi=300, bbox_inches='tight')
+    #plt.show()
+    fig.savefig(outputFile, dpi=300, bbox_inches='tight')
 
 
 
@@ -197,10 +245,9 @@ if __name__ == '__main__':
     parallelExecutorID = 30
     cmsExecutorID = 17
     g1ExecutorID = 16
-    #plotHeapUsage(appName, inputFile + "Parallel/parallel-E" + str(parallelExecutorID) + "-parsed.txt",
-    #              "parallel-E" + str(parallelExecutorID) + ".pdf")
-    #plotHeapUsage(appName, inputFile + "CMS/CMS-E" + str(cmsExecutorID) + "-parsed.txt", "CMS-E" + str(cmsExecutorID) + ".pdf")
-    plotHeapUsage(appName, inputFile + "G1/G1-E" + str(g1ExecutorID) + "-parsed.txt", "G1-E" + str(g1ExecutorID) + ".pdf")
+    plotHeapUsage(appName, inputFile + "Parallel/parallel-E" + str(parallelExecutorID) + "-parsed.txt", inputFile + "Parallel/parallel-E" + str(parallelExecutorID) + ".pdf")
+    plotHeapUsage(appName, inputFile + "CMS/CMS-E" + str(cmsExecutorID) + "-parsed.txt", inputFile + "CMS/CMS-E" + str(cmsExecutorID) + ".pdf")
+    plotHeapUsage(appName, inputFile + "G1/G1-E" + str(g1ExecutorID) + "-parsed.txt", inputFile + "G1/G1-E" + str(g1ExecutorID) + ".pdf")
 
 
 
